7486b0d9162a8c8812d98c592c3230228d30cc52
Austin Chang <austinlchang@gmail.com>
2013-04-18 01:05:46 -0700

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,35 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have the title 'Home'" do
+    	visit '/static_pages/home'
+    	expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
+
+describe "Help page" do
+
+	it "should have content 'Help'" do
+		visit '/static_pages/help'
+		expect(page).to have_content('Help')
+	end
+	it "should have title 'Help'" do
+		visit '/static_pages/help'
+		expect(page).to have_title("Help")
+	end
+end
+
+describe "About page" do
+	it "should have the content 'About Us'" do
+		visit '/static_pages/about'
+		expect(page). to have_content('About Us')
+	end
+	it "should have title 'About'" do
+		visit '/static_pages/about'
+		expect(page).to have_title("About Us")
+	end
+end
+
+
+
 end
