898d22c9f09af724903a14087dd110233ceb56c4
Austin Chang <austinlchang@gmail.com>
2013-05-09 14:27:01 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -43,11 +43,11 @@ end
 
 describe "Contact page" do
 	it "should have content 'Contact Us'" do
-		visit '/static_pages/contact'
+		visit 'contact_path'
 		expect(page).to have_content('Contact Us')
 	end
 	it "should have title 'Contact Us'" do
-		visit '/static_pages/contact'
+		visit 'contact_path'
 		expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact Us")
 	end
 
