66c3c8f6b125442ae5fe9a34066404e1283b4a9f
Austin Chang <austinlchang@gmail.com>
2013-06-16 01:37:40 -0700

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,8 +18,36 @@ describe User do
  		 it { should respond_to(:authenticate) }
  		 it { should respond_to(:admin) }
  		 it { should respond_to(:microposts) }
+ 		 it { should respond_to(:feed) }
+ 		 it { should respond_to(:relationships) }
+ 		 it { should respond_to(:followed_users) }
+ 		 it { should respond_to(:reverse_relationships) }
+ 		 it { should respond_to(:followers) }
 
   		 it { should be_valid }
+
+  	describe "following/unfollowing stuff" do
+  		let(:other_user) { FactoryGirl.create(:user) }
+  		before do
+  			@user.save
+  			@user.follow!(other_user)
+  		end
+
+  		it { should be_following(other_user) }
+  		its(:followed_users) { should include(other_user) }
+
+  		describe "followed user" do
+  			subject { other_user }
+  			its(:followers) { should include(@user) }
+  		end
+
+  		describe "and unfollowing" do
+  			before { @user.unfollow!(other_user) }
+
+  			it { should_not be_following(other_user) }
+  			its(:followed_users) { should_not include(other_user) }
+  		end
+  	end
 
   	describe "micropost associations" do
 
@@ -49,91 +77,97 @@ describe User do
   			let(:unfollowed_post) do
   				FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
   			end
+  			let(:followed_user) { FactoryGirl.create(:user) }
+
+  			before do
+  				@user.follow!(followed_user)
+  				3.times { followed_user.microposts.create!(content: "lorem ipsum") }
+  			end
 
   			its(:feed) { should include(newer_micropost) }
   			its(:feed) { should include(older_micropost) }
   			its(:feed) { should_not include(unfollowed_post) }
+  			its(:feed) do
+  				followed_user.microposts.each do |micropost|
+  					should include(micropost)
+  				end
+  			end
   		end
-  		
-  		
   	end
 
+  	describe "sign-up-stuff" do
+		describe "when name is not present" do
+	    	before { @user.name = " " }
+	    	it { should_not be_valid }
+		end 
 
+	 	describe "when email is not present" do
+	    	before { @user.email = " " }
+	    	it { should_not be_valid }
+		end
 
-	describe "when name is not present" do
-    	before { @user.name = " " }
-    	it { should_not be_valid }
+		describe "when name is too long" do
+	    	before { @user.name = "a" * 51 }
+	    	it { should_not be_valid }
+		end
 
-	end 
+		describe "when email format is invalid" do
+	  		it "should be invalid" do
+	  			addresses = %w[user@foo,com user_at_foo.org example.user@foo.
+	                     foo@bar_baz.com foo@bar+baz.com]
+	    	    addresses.each do |invalid_address|
+	       			@user.email = invalid_address
+	       			expect(@user).not_to be_valid
+		        end
+		    end
+		end
 
- 	describe "when email is not present" do
-    	before { @user.email = " " }
-    	it { should_not be_valid }
-
-	end
-
-	describe "when name is too long" do
-    	before { @user.name = "a" * 51 }
-    	it { should_not be_valid }
-
-	end
-
-	describe "when email format is invalid" do
-  		it "should be invalid" do
-  			addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
-    	    addresses.each do |invalid_address|
-       			@user.email = invalid_address
-       			expect(@user).not_to be_valid
-	        end
-	    end
-	end
-
-	describe "when email format is valid" do
-		it "should be valid" do
-			addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-			addresses.each do |valid_address|
-				@user.email = valid_address
-				expect(@user).to be_valid
+		describe "when email format is valid" do
+			it "should be valid" do
+				addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+				addresses.each do |valid_address|
+					@user.email = valid_address
+					expect(@user).to be_valid
+				end
 			end
 		end
-	end
-	
-	describe "when email address is already taken" do
-		before do
-			user_with_same_email = @user.dup
-			user_with_same_email.email = @user.email.upcase
-			user_with_same_email.save
+		
+		describe "when email address is already taken" do
+			before do
+				user_with_same_email = @user.dup
+				user_with_same_email.email = @user.email.upcase
+				user_with_same_email.save
+			end
+
+			it { should_not be_valid }
 		end
 
-		it { should_not be_valid }
-	end
+		describe "when password is not present" do
+		    before do
+		      @user = User.new(name: "Example User", email: "user@example.com", 
+	    	                password: " ", password_confirmation: " ")
+	    	end
+	    	it { should_not be_valid }
+	  	end
 
-	describe "when password is not present" do
-	    before do
-	      @user = User.new(name: "Example User", email: "user@example.com", 
-    	                password: " ", password_confirmation: " ")
-    	end
-    	it { should_not be_valid }
-  	end
+		describe "when password doesn't match confirmation" do 
+			before { @user.password_confirmation = "mismatch" }
+			it { should_not be_valid }
 
-	describe "when password doesn't match confirmation" do 
-		before { @user.password_confirmation = "mismatch" }
-		it { should_not be_valid }
-
-	end
-
-	describe "when password confirmation is nil" do 
-		before do 
-			@user = User.new(name: "Michael Hartl", email: "mhartl@example.com",
-							password: "foobar", password_confirmation: nil)
 		end
-		it { should_not be_valid }
-	end
 
-	describe "with a password that's too short" do
-		before { @user.password = @user.password_confirmation = "a" * 5 }
-		it { should be_invalid }
+		describe "when password confirmation is nil" do 
+			before do 
+				@user = User.new(name: "Michael Hartl", email: "mhartl@example.com",
+								password: "foobar", password_confirmation: nil)
+			end
+			it { should_not be_valid }
+		end
+
+		describe "with a password that's too short" do
+			before { @user.password = @user.password_confirmation = "a" * 5 }
+			it { should be_invalid }
+		end
 	end
 
 	describe "return value of authenticate method" do
